157472163019f7bf03185ad26e5e79369f2a20a1
alex <alex7ey1@gmail.com>
2017-12-24 18:34:16 +0300

    Re-arrange

--- src/main/java/processorejb/ProcessorBean.java
+++ src/main/java/processorejb/ProcessorBean.java
@@ -1,34 +1,56 @@
 package processorejb;
 
-import processorejb.persistence.EOrderEntity;
-import processorejb.util.Utils;
+import entities.EOrderEntity;
 
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.MessageDriven;
+import javax.annotation.Resource;
+import javax.ejb.*;
 import javax.jms.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.*;
 
 @MessageDriven(activationConfig = {
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
         @ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/jms/queue/txrequest"),
         @ActivationConfigProperty(propertyName = "maxSession", propertyValue = "10")
 })
+@TransactionManagement(value = TransactionManagementType.CONTAINER)
 public class ProcessorBean implements MessageListener {
+
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Resource
+    UserTransaction utx;
 
 
     public ProcessorBean() {
     }
 
     @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public void onMessage(Message message) {
 
         ObjectMessage message1 = (ObjectMessage) message;
 
         try {
 
-            new Utils().saveToDB((EOrderEntity) message1.getObject());
-        } catch (JMSException e) {
+
+            EOrderEntity entity = (EOrderEntity) message1.getObject();
+            utx.begin();
+
+//            entityManager.joinTransaction();
+
+            entityManager.persist(entity);
+
+            utx.commit();
+
+            entityManager.close();
+
+
+        } catch (JMSException | NotSupportedException | SystemException | HeuristicMixedException | RollbackException | HeuristicRollbackException e) {
             e.printStackTrace();
         }
-
     }
 }
